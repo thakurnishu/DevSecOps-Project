dfd0b6f0fd767ff0078d7b3b921c5416ab03c1dc
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-01 18:00:20 +0530

    corrected

--- SetupScripts/CICD/Install-Docker.sh
+++ SetupScripts/CICD/Install-Docker.sh
@@ -6,6 +6,5 @@ sudo apt-get install docker.io -y
 sudo usermod -aG docker $USER  # Replace with your system's username, e.g., 'ubuntu'
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
-sleep 1
 echo 
 echo "Docker is Installed"
